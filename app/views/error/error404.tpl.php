7f660f00c9b26a13bafc26df584d0196c9d25620
Aurozia <valentineoay@gmail.com>
2024-01-25 19:56:41 +0100

    End - For now

--- app/views/error/error404.tpl.php
+++ app/views/error/error404.tpl.php
@@ -1,12 +1,12 @@
 <section class="hero">
-    <div class="container">
-        <!-- Breadcrumbs -->
-        <ol class="breadcrumb justify-content-center">
-            <li class="breadcrumb-item"><a href="<?= $router->generate('main-home'); ?>">HOME</a></li>
-        </ol>
-        <!-- Hero Content-->
-        <div class="hero-content pb-5 text-center">
-            <h1 class="hero-heading">Chemin inconnu !</h1>
-        </div>
+  <div class="container">
+    <!-- Breadcrumbs -->
+    <ol class="breadcrumb justify-content-center">
+      <li class="breadcrumb-item"><a href="<?= $router->generate('main-home'); ?>">HOME</a></li>
+    </ol>
+    <!-- Hero Content-->
+    <div class="hero-content pb-5 text-center">
+      <h1 class="hero-heading">Chemin inconnu !</h1>
     </div>
+  </div>
 </section>
